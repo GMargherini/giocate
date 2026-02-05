dafdc27000f80a440753e90330ba0f544a15867f
GMargherini <giorgio.margherini@gmail.com>
2026-02-05 14:10:31 +0100

    added weekday chart

--- src/pages/dashboard.py
+++ src/pages/dashboard.py
@@ -1,9 +1,16 @@
 from nicegui import ui
+import datetime
+import matplotlib.pyplot as plt
 from data.game import Game
 
 def dashboard_page(giocate: list[Game]):
     months = list(set([m["date"].split("-")[1] for m in giocate]))
     month_values = [sum(g["win"] for g in giocate if g["date"].split("-")[1] == m) for m in months]
+
+    weekdays = ["lun", "mar", "mer", "gio", "ven", "sab", "dom"]
+    weekday_values = [sum(g["win"] for g in giocate if datetime.datetime.strptime(g["date"], "%Y-%m-%d").weekday() == wd) for wd in range(0,7)]
+    weekdays_all = filter(lambda x: x[1] != 0,zip(weekdays, weekday_values))
+    weekdays, weekday_values = zip(*weekdays_all)
     
     tot = sum([i["result"] for i in giocate])
     perc = sum([1 for i in giocate if i["result"] > 0]) / sum([1 for _ in giocate]) * 100
@@ -24,11 +31,16 @@ def dashboard_page(giocate: list[Game]):
         with ui.row(align_items="center").classes("w-full"):
             with ui.matplotlib(figsize=(3, 2)).classes("flex-auto").figure as fig:
                 ax = fig.gca()
+                ax.set_title("Numero vittorie")
+                ax.pie(win_lose, labels=["Vincite", "Perdite"], colors=["lightgreen", "#F67280"])
+        with ui.row(align_items="center").classes("w-full"):
+
+            with ui.matplotlib(figsize=(3, 2)).classes("flex-auto").figure as fig:
+                ax = fig.gca()
                 ax.set_title("Vittorie per mese")
                 ax.pie(month_values, labels=months)
 
             with ui.matplotlib(figsize=(3, 2)).classes("flex-auto").figure as fig:
                 ax = fig.gca()
-                ax.set_title("Numero vittorie")
-                ax.pie(win_lose, labels=["Vincite", "Perdite"], colors=["lightgreen", "#F67280"])
-
+                ax.set_title("Vittorie per giorno della settimana")
+                ax.pie(weekday_values, labels=weekdays)
